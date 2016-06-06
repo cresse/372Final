7b2318bf6f859d8b976230be0118e29950b8343e
Cresse <ijcresse@gmail.com>
2016-06-06 14:34:37 -0700

    transposed cacheconfig.cc and .txt

---
+++ src/CacheConfig.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class CacheConfig {
+	public CacheConfig() {
+		
+	}
+	
+	public CacheConfig(File output) {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
+}
